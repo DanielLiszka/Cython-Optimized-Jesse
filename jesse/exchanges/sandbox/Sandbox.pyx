4f32cb348ad63e3626038b7f56c28ec0792f49a8
Daniel Liszka <liszkadp@alumni.vcu.edu>
2022-11-18 21:09:54 -0500

    added exchanges

---
+++ jesse/exchanges/sandbox/Sandbox.pyx
@@ -0,0 +1,82 @@
+import sys
+import jesse.helpers as jh
+from jesse.enums import order_types
+from jesse.exchanges.exchange import Exchange
+from jesse.models import Order
+from jesse.store import store
+from typing import Union
+import random
+def uuid4():
+  s = '%032x' % random.getrandbits(128)
+  return s[0:8]+'-'+s[8:12]+'-4'+s[13:16]+'-'+s[16:20]+'-'+s[20:32]
+  
+class Sandbox(Exchange):
+    def __init__(self, name='Sandbox'):
+        super().__init__()
+        self.name = name
+
+    def market_order(self, symbol: str, qty: float, current_price: float, side: str, reduce_only: bool) -> Order:
+        order = Order({
+            'id': uuid4(),
+            'symbol': symbol,
+            'exchange': self.name,
+            'side': side,
+            'type': order_types.MARKET,
+            'reduce_only': reduce_only,
+            'qty': jh.prepare_qty(qty, side),
+            'price': current_price,
+        })
+
+        store.orders.add_order(order)
+
+        store.orders.to_execute.append(order)
+
+        return order
+
+    def limit_order(self, symbol: str, qty: float, price: float, side: str, reduce_only: bool) -> Order:
+        order = Order({
+            'id': uuid4(),
+            'symbol': symbol,
+            'exchange': self.name,
+            'side': side,
+            'type': order_types.LIMIT,
+            'reduce_only': reduce_only,
+            'qty': jh.prepare_qty(qty, side),
+            'price': price,
+        })
+
+        store.orders.add_order(order)
+
+        return order
+
+    def stop_order(self, symbol: str, qty: float, price: float, side: str, reduce_only: bool) -> Order:
+        order = Order({
+            'id': uuid4(),
+            'symbol': symbol,
+            'exchange': self.name,
+            'side': side,
+            'type': order_types.STOP,
+            'reduce_only': reduce_only,
+            'qty': jh.prepare_qty(qty, side),
+            'price': price,
+        })
+
+        store.orders.add_order(order)
+
+        return order
+
+    def cancel_all_orders(self, symbol: str) -> None:
+        orders = filter(lambda o: o.is_new,
+                        store.orders.get_orders(self.name, symbol))
+
+        for o in orders:
+            o.cancel()
+
+        if not "pytest" in sys.modules:
+            store.orders.storage[f'{self.name}-{symbol}'].clear()
+
+    def cancel_order(self, symbol: str, order_id: str) -> None:
+        store.orders.get_order_by_id(self.name, symbol, order_id).cancel()
+
+    def _fetch_precisions(self) -> None:
+        pass
